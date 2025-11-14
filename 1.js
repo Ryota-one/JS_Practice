759c420dca34b5650f7c42f6169c4023089464e2
Ryota Matsuda <omatsudane@yahoo.co.jp>
2025-11-14 17:14:27 +0900

    How to use DOM

--- 1.js
+++ 1.js
@@ -21,18 +21,48 @@ console.log(catchFish(10,2));
 
 // const calc = (num,func) => {
 //     return func(num);
+// // }
+
+// const plus1 =(num) =>{
+//     return num + 1;
 // }
 
-const plus1 =(num) =>{
-    return num + 1;
-}
+// const calc = (num,func) => {
+//     return func(num);
+// }
 
-const calc = (num,func) => {
-    return func(num);
-}
+// console.log(calc(5,plus1));
 
-console.log(calc(5,plus1));
-console.log(calc(5,(num) => {
-    return num * 2;
-}));
-console.log(calc(5,num => num * 3));
+// console.log(calc(5,(num) => {
+//     return num * 2;
+// }));
+
+// console.log(calc(5,num => num * 3));
+
+// const scores = [10,20,30,130,423];
+
+// scores.forEach((score)=>{
+//     console.log(score);
+// })
+
+// const nums = [1,2,3,4,5];
+// const numsBackup = [...nums];
+
+// nums.push(10);
+
+// console.log(nums);
+// console.log(numsBackup);
+
+
+//document.querySelectorを使ってHTML要素を取得可能
+//textContentプロパディを使用してテキストを変更が可能
+
+document.querySelector('button').addEventListener('click',() => {
+    document.querySelector('p').textContent = 'yaay'
+});
+
+// classListを使ってCSSスタイルを取得が可能。
+
+document.querySelector('button').addEventListener('click', () => {
+    document.querySelector('p').classList.add('pink-bg');
+});
