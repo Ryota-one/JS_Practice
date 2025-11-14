66f232be9a29b8da630ff19a016ef8e065e1a55c
Ryota Matsuda <omatsudane@yahoo.co.jp>
2025-11-14 11:02:31 +0900

    Just practice

---
+++ 1.js
@@ -0,0 +1,35 @@
+'use strict'
+{
+    function example(cul){
+        return cul*5;
+    }
+    console.log(example(5));
+}
+
+
+
+const catchFish = (kijihata,seabass) => {
+    return kijihata + seabass;
+}
+console.log(catchFish(10,2));
+
+
+
+// const double = (num) =>{
+//     return num * 2;
+// };
+
+// const calc = (num,func) => {
+//     return func(num);
+// }
+
+const plus1 =(num) =>{
+    return num + 1;
+}
+
+const calc = (num,func) => {
+    return func(num);
+}
+
+console.log(calc(5,plus1));
+
